4b82c33b59684ca5e4b9c0d41b7aa4edcd458332
Vvisal <visalseng963@gmail.com>
2023-12-14 16:51:18 +0700

    Create constructure

--- employee.cpp
+++ employee.cpp
@@ -6,5 +6,19 @@ class employee{
     private:
         int id;
         string name;
+        string department;
+        double salary;
     public:
+        employee(){
+            id = 0;
+            name = "Unknown";
+            department = "Unknown";
+            salary = 0.00;
+        }
+        employee(int id,string name,string department,double salary){
+            this->id = id;
+            this->name = name;
+            this->department = department;
+            this->salary=salary;
+        }
 };
